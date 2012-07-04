608cf86e6a7c0640ddb22782546ca86826cbeb91
Alex Thomas <alex.c.thomas@gmail.com>
2012-07-04 12:37:25 +0100

    refactored for vimeo

---
+++ app/models/youtuber/feeds/youtube/user_feed.rb
@@ -0,0 +1,64 @@
+module Youtuber
+  module Feeds
+    module Youtube
+      class UserFeed < YoutubeFeed
+      
+        @queue = :user_feed_queue
+      
+        attr_reader :user
+      
+        def initialize(params,options = {})
+          super params
+          @url = base_url
+          @url << "#{params[:user]}/"
+        
+          user_playlist = params[:playlist] && params[:playlist] == 'favourites' ? 'favourites' : 'uploads'  
+          @url << user_playlist
+          @url << build_query_params(to_youtube_params)
+          Rails.logger.debug "feed url: #{@url}"
+        
+        
+        end
+      
+        def base_url
+          super << "users/"
+        end
+      
+        def parse
+         
+        end
+      
+        def self.enqueue_feed feed
+          Resque.enqueue(feed.class, feed.user,feed.url)
+        end
+      
+        def self.perform(user,url)
+          videos = []
+          end_parse = false
+          fp = Youtuber::FeedParser.new url
+          fp.parse do | parser |
+            parser.response.entries.each do | entry |
+              videos << parser.parse_video(entry)
+
+              if Youtuber::Video.video_exists?(videos.last.video_id)
+                end_parse = true
+                break
+              end
+              videos.last.save!    
+            end
+            end_parse = parser.response.next_page.nil? 
+            if !end_parse
+              nf = Youtuber::Feeds::Youtube::UserFeed.new(:user => user,:offset => parser.response.next_offset, :items_per_page => parser.response.items_per_page)
+              Rails.logger.debug "next feed url #{nf.url}"
+              #self.enqueue_feed nf
+            end
+          end
+        end
+                
+      end
+    end
+   
+  end
+  
+end
+
